5e046a70703e3b232558d856a193525fe63d2c12
Nassima KHOULA <nassima.khoula@gmail.com>
2023-10-12 10:42:18 +0200

    remise à 0

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Home from './home';
-import About from './about';
-import NotFound from './NotFound';
-import AppRouter from './AppRouter';
-import Layout from './layout';
-import './Layout.scss';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <AppRouter>
-      <Layout>
-        <App />
-        <Home />
-        <About />
-        <NotFound />
-      </Layout>
-    </AppRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
+
 reportWebVitals();
+
